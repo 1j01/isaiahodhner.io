82b37682501e9b97d1a092e8228c74374e1df80d
Isaiah Odhner <isaiahodhner@gmail.com>
2018-03-14 07:48:23 +0000

    Clean up variable names

--- extensionless-urls.js
+++ extensionless-urls.js
@@ -1,22 +1,21 @@
-
 var fs = require('fs');
 var path = require('path');
-var public_dir = '';
-var exts = ['html'];
+var rootDirectory = '';
+var extensions = ['html'];
 module.exports = function (req, res, next) {
 	if (req.method !== 'GET' && req.method !== 'HEAD') {
 		return next();
 	}
 	if (req.url !== '/' && path.extname(req.url) === '') {
-		var url_path = req.url.replace('/', '');
+		var requestedPath = req.url.replace('/', '');
 		var i = 0;
 		var check = function () {
-			fs.exists(public_dir + url_path + '.' + exts[i], function (exists) {
+			fs.exists(rootDirectory + requestedPath + '.' + extensions[i], function (exists) {
 				if (exists) {
-					req.url += '.' + exts[i];
+					req.url += '.' + extensions[i];
 					next();
 				} else {
-					if (++i >= exts.length) {
+					if (++i >= extensions.length) {
 						next();
 					} else {
 						check();
