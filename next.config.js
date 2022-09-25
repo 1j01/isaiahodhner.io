90af2fbaa30bb0cc96dd78a1122a106c8a388ba2
Isaiah Odhner <isaiahodhner@gmail.com>
2022-09-24 23:05:17 -0400

    Add type info for Next config
    It's not quite a schema, it doesn't highlight invalid keys, but it gives some autocomplete at least.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
-module.exports = {
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
 	env: {
 		tileSizesByProjectRepoName: require("./src/get-tile-sizes-data")(),
-	}
+	},
 };
+
+module.exports = nextConfig;
