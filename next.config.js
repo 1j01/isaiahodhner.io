8401fa90998e093f6b28f9fd39df2eaa2dac6eb4
Isaiah Odhner <isaiahodhner@gmail.com>
2025-04-30 22:56:35 -0400

    Set up @svgr/webpack for importing SVG files as components

--- next.config.js
+++ next.config.js
@@ -2,5 +2,29 @@ module.exports = {
 	output: 'export',
 	env: {
 		tileSizesByProjectRepoNameJSON: JSON.stringify(require("./src/get-tile-sizes-data")()),
-	}
+	},
+	webpack(config) {
+		const fileLoaderRule = config.module.rules.find((rule) =>
+			rule.test?.test?.(".svg")
+		);
+
+		config.module.rules.push(
+			{
+				...fileLoaderRule,
+				test: /\.svg$/i,
+				resourceQuery: /url/,
+			},
+
+			{
+				test: /\.svg$/i,
+				issuer: fileLoaderRule.issuer,
+				resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+				use: ["@svgr/webpack"],
+			}
+		);
+
+		fileLoaderRule.exclude = /\.svg$/i;
+
+		return config;
+	},
 };
