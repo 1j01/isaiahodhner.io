77a58463fb74b172754f643833e4a05d3e114f70
Isaiah Odhner <isaiahodhner@gmail.com>
2025-08-15 08:47:55 -0400

    Fix tile spanning when running/deploying on Windows

--- src/get-tile-sizes-data.cjs
+++ src/get-tile-sizes-data.cjs
@@ -4,7 +4,13 @@ const path = require("path");
 const getTileSizesData = () => {
 	const tileSizesByProjectRepoName = {};
 	const tileImagesFolder = path.resolve("public/images/projects");
-	for (let img_path of glob.sync(path.join(tileImagesFolder, `*.png`))) {
+	// Handling Windows is annoying
+	// glob.sync(path.join(tileImagesFolder, `*.png`), { windowsPathsNoEscape: true })
+	const img_paths = glob.sync("*.png", { cwd: tileImagesFolder, absolute: true });
+	if (img_paths.length === 0) {
+		throw new Error("No image paths found");
+	}
+	for (let img_path of img_paths) {
 		const match = img_path.match(/[/\\]([^/\\]+)-(\d+x\d+)\.png/);
 		if (match) {
 			const [, repoName, size] = match;
